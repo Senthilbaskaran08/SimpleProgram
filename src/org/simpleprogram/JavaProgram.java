a0ecc7b589f69feac30b13761e99e123b669c7ce
Senthil <senthilbaskaran22@gmail.com>
2019-09-17 11:09:09 +0530

    change made in login2

--- src/org/simpleprogram/JavaProgram.java
+++ src/org/simpleprogram/JavaProgram.java
@@ -5,10 +5,15 @@ static void program() {
 	String s = "Senthil";
 	System.out.println(s);
 }
-	
+static void program2() {
+	String s = "Senthil2";
+	System.out.println(s);
+}
 public static void main(String[] args) {
 	JavaProgram obj = new JavaProgram();
 	obj.program();
+	obj.program2();
 }
-}
+}	
+
  
